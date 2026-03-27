0db51e688b04e7aadebffdfec082c9757df12a35
Kyosuke01 <fire.gaming95800@gmail.com>
2026-03-27 14:06:49 +0100

    Mise en place de la structure complète (+ BDD init)

---
+++ backend/db/init.sql
@@ -0,0 +1,66 @@
+CREATE TABLE Student (
+  student_id INT PRIMARY KEY AUTO_INCREMENT,
+  n_admin_id VARCHAR(100) PRIMARY KEY AUTO_INCREMENT,
+  student_name VARCHAR(100),
+  mail_student VARCHAR(255),
+  password VARCHAR(255),
+  class_id INT /* FOREIGN KEY */,
+  dob DATETIME
+);
+
+CREATE TABLE Teacher (
+  teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+  teacher_name VARCHAR(100),
+  topic_id INT /* FOREIGN KEY */
+);
+
+CREATE TABLE Room (
+  room_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50)
+);
+
+CREATE TABLE Topic (
+  topic_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) PRIMARY KEY,
+  teacher_id INT /* FOREIGN KEY */,
+  class_id INT /* FOREIGN KEY */
+);
+
+CREATE TABLE Class (
+  class_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) PRIMARY KEY,
+  student_id INT /* FOREIGN KEY */,
+  teacher_id INT /* FOREIGN KEY */
+);
+
+CREATE TABLE Grade (
+  grade_id INT PRIMARY KEY AUTO_INCREMENT,
+  grade FLOAT,
+  student_id INT /* FOREIGN KEY */,
+  topic_id INT /* FOREIGN KEY */
+);
+
+CREATE TABLE EDT (
+  edt_id INT PRIMARY KEY AUTO_INCREMENT,
+  topic_id INT /* FOREIGN KEY */,
+  room_id INT /* FOREIGN KEY */,
+  teacher_id INT /* FOREIGN KEY */,
+  class_id INT /* FOREIGN KEY */,
+  start_time DATETIME,
+  end_time DATETIME
+);
+
+CREATE TABLE Attendance (
+  attendance_id INT PRIMARY KEY AUTO_INCREMENT,
+  late INT,
+  absent INT,
+  student_id INT /* FOREIGN KEY */,
+  topic_id INT /* FOREIGN KEY */
+);
+
+CREATE TABLE TICKET (
+  ticket_id INT PRIMARY KEY AUTO_INCREMENT,
+  msg_content VARCHAR(255),
+  teacher_id INT /* FOREIGN KEY */,
+  student_id INT /* FOREIGN KEY */
+);
