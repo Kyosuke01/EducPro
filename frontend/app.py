39c3cad6d1d00e4b6670771555b958d4cfdcfc85
Kyosuke01 <fire.gaming95800@gmail.com>
2026-03-27 13:37:18 +0100

    correction problèmes - structure correcte (test des connextions frontend / admin / backend)

--- frontend/app.py
+++ frontend/app.py
@@ -6,8 +6,8 @@ API_URL = "http://backend:5000"
 
 @app.route("/")
 def index():
-    courses = requests.get(f"{API_URL}/api/courses").json()
-    return render_template("index.html", courses=courses)
+    users = requests.get(f"{API_URL}/api/users/").json()
+    return render_template("index.html", users=users)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
